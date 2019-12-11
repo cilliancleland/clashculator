95b3feaf1c9df681e377e3fc6989b267cb2fa9d0
Cillian Cleland <cillian.cleland@winc.com.au>
2019-12-11 16:53:49 +1100

    Adding build 20191211

---
+++ precache-manifest.e1a6e9b4d22fcfd4d044049a49017952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e8e7f245f0fbbb7dde",
+    "url": "css/app.3b6760ef.css"
+  },
+  {
+    "revision": "2cc5aef1fcd89c3645447a9891bd8d53",
+    "url": "index.html"
+  },
+  {
+    "revision": "78e8e7f245f0fbbb7dde",
+    "url": "js/app.fd4db07a.js"
+  },
+  {
+    "revision": "7b79fe18be52e9052252",
+    "url": "js/chunk-vendors.a002552d.js"
+  },
+  {
+    "revision": "9be3cc45caee054938ab78b574405cf2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.e1a6e9b4d22fcfd4d044049a49017952.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
