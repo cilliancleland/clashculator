859fc5ea1411dbb2db3b25b13587a2c9d84755bc
User-PC\User <cilliancleland@gmail.com>
2019-12-23 11:07:17 +1100

    Lists

---
+++ precache-manifest.19ba5def9dbf74af9d4d0572c3d457c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e40766544f40557ef38",
+    "url": "css/app.2270f3ff.css"
+  },
+  {
+    "revision": "ee1ed7452252b92d1e80fef7d86404e6",
+    "url": "index.html"
+  },
+  {
+    "revision": "1e40766544f40557ef38",
+    "url": "js/app.9fff219f.js"
+  },
+  {
+    "revision": "8d0fc893ce8e855285d9",
+    "url": "js/chunk-vendors.dc56ee3f.js"
+  },
+  {
+    "revision": "9be3cc45caee054938ab78b574405cf2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
