43305e78a5716b4879755fea9588bb871cbbfcd7
User-PC\User <cilliancleland@gmail.com>
2019-12-23 11:05:17 +1100

    test build

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.e47932979e5561dba3857f0d87d61547.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.19ba5def9dbf74af9d4d0572c3d457c5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
