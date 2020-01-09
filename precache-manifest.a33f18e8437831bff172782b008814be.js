b9e3b671f13bb9223c80f8d4788eb365d9686409
Cillian Cleland <cillian.cleland@winc.com.au>
2020-01-09 11:06:23 +1100

    Hopefully fix FB link issue

---
+++ precache-manifest.a33f18e8437831bff172782b008814be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4863f2e4767a735f71ab",
+    "url": "css/app.85ad104d.css"
+  },
+  {
+    "revision": "732787dc5acc5e20d4ba5d05227a3ebb",
+    "url": "index.html"
+  },
+  {
+    "revision": "4863f2e4767a735f71ab",
+    "url": "js/app.28ab62bb.js"
+  },
+  {
+    "revision": "5b72467d9000040974ee",
+    "url": "js/chunk-vendors.e8d49539.js"
+  },
+  {
+    "revision": "9be3cc45caee054938ab78b574405cf2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "6978a616c585d03cb5b542a891995efb",
+    "url": "robots.txt"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.e47932979e5561dba3857f0d87d61547.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.a33f18e8437831bff172782b008814be.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
