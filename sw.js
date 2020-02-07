2f35d96be9eb466d021f71245281b5b4aeb62ba7
Cillian Cleland <cillian.cleland@winc.com.au>
2020-02-07 16:07:05 +1100

    compact lists

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 importScripts("precache-manifest.2fa1781806a4bd0bfe41965d2686cf48.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+=======
+importScripts("precache-manifest.ad37aca8e7eaa993a073657244d29ee7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+>>>>>>> Stashed changes
 
 
