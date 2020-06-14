839de0db1fe07cf7430baf0a0b9c66e78a7c2460
unknown <cilliancleland@gmail.com>
2020-06-14 21:17:14 +1000

    Fix default number

--- precache-manifest.0990564673f4bd8ae5e648a685d06d3a.js
+++ precache-manifest.0990564673f4bd8ae5e648a685d06d3a.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76117b57753e3e9d89d9",
+    "revision": "8aafee942fc421a1cc15",
     "url": "css/app.48d3ebc1.css"
   },
   {
-    "revision": "f1a5722b75bc0b7d63a18701e004d273",
+    "revision": "7b751ccd1e00262fb12c64c8ac8ec860",
     "url": "index.html"
   },
   {
-    "revision": "76117b57753e3e9d89d9",
-    "url": "js/app.3d5be93f.js"
+    "revision": "8aafee942fc421a1cc15",
+    "url": "js/app.62aab028.js"
   },
   {
     "revision": "d4b487cb721e114a70cb",
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.336e0ce70f77ce98d027a969d2b9be41.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.0990564673f4bd8ae5e648a685d06d3a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
