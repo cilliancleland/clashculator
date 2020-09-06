2821de03331a3386e62462b9cd393c0ff7540a04
unknown <cilliancleland@gmail.com>
2020-09-06 13:58:06 +1000

    options

---
+++ precache-manifest.2fe357a1eedab279cf816ad0cf6c3524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5e06a480fb1569e01d",
+    "url": "css/app.1b4c4548.css"
+  },
+  {
+    "revision": "0f0bcc4f857907ac4aaf981020af4de2",
+    "url": "index.html"
+  },
+  {
+    "revision": "ad5e06a480fb1569e01d",
+    "url": "js/app.2496b751.js"
+  },
+  {
+    "revision": "84deaf73e1dc34b01b43",
+    "url": "js/chunk-vendors.8a37bdfa.js"
+  },
+  {
+    "revision": "e53898dd62fec8fbaa2315bab9d0a170",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "6978a616c585d03cb5b542a891995efb",
+    "url": "robots.txt"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.5ef1750997be83cb8eab70b062e4b23b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.2fe357a1eedab279cf816ad0cf6c3524.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
