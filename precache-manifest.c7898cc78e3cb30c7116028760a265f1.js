89f7dc69cf78ae761f6c8993617bfdf6be05ad67
unknown <cilliancleland@gmail.com>
2021-01-30 20:12:38 +1100

    samurai characters

--- precache-manifest.c7898cc78e3cb30c7116028760a265f1.js
+++ precache-manifest.c7898cc78e3cb30c7116028760a265f1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5aba706fb710a27ebe7",
+    "revision": "62c2265c6bcdcb753e39",
     "url": "css/app.cf6e75dc.css"
   },
   {
-    "revision": "4600edd990994e91369e76f630e9330a",
+    "revision": "5c2286af18cbdbd78600ee75d440bfe2",
     "url": "index.html"
   },
   {
-    "revision": "d5aba706fb710a27ebe7",
-    "url": "js/app.91ee269f.js"
+    "revision": "62c2265c6bcdcb753e39",
+    "url": "js/app.92954d8c.js"
   },
   {
-    "revision": "84deaf73e1dc34b01b43",
-    "url": "js/chunk-vendors.8a37bdfa.js"
+    "revision": "60baa2b95c2cc4dcface",
+    "url": "js/chunk-vendors.bc5577d2.js"
   },
   {
     "revision": "e53898dd62fec8fbaa2315bab9d0a170",
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.7ca2ff659c40f90231f09146ba145f9f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.c7898cc78e3cb30c7116028760a265f1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
