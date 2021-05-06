e0e6f41451cb0d40ae3e593c724ec223cfb136fc
unknown <cilliancleland@gmail.com>
2021-05-06 12:45:20 +1000

    elephant attacks

--- precache-manifest.31033519374907eb30400a0f612e3f98.js
+++ precache-manifest.31033519374907eb30400a0f612e3f98.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4969c8f8820ca60e443",
+    "revision": "4222eacd6927d5562042",
     "url": "css/app.9118d87e.css"
   },
   {
-    "revision": "e76c17f216d62a6bc098f34f1e51a1df",
+    "revision": "f015413ac6c7a53d370effe133f5b44b",
     "url": "index.html"
   },
   {
-    "revision": "e4969c8f8820ca60e443",
-    "url": "js/app.fa450746.js"
+    "revision": "4222eacd6927d5562042",
+    "url": "js/app.fd14c96f.js"
   },
   {
     "revision": "60baa2b95c2cc4dcface",
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.977294463a2060ef60702696a465d393.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.31033519374907eb30400a0f612e3f98.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
