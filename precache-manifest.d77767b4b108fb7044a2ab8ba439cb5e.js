1c49fddb2292dab30cb23abd31b11b0f2a6de6ae
unknown <cilliancleland@gmail.com>
2021-10-19 21:19:10 +1100

    minor fixes

--- precache-manifest.d77767b4b108fb7044a2ab8ba439cb5e.js
+++ precache-manifest.d77767b4b108fb7044a2ab8ba439cb5e.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b7def9120689ceef7e6",
+    "revision": "b581fd180b59e03b51ee",
     "url": "css/app.30f31967.css"
   },
   {
-    "revision": "ae2eaf5513725f40da24612c37518a00",
+    "revision": "7e1b77b24299508853e5fa3f832499f0",
     "url": "index.html"
   },
   {
-    "revision": "3b7def9120689ceef7e6",
-    "url": "js/app.8f3a0477.js"
+    "revision": "b581fd180b59e03b51ee",
+    "url": "js/app.95074760.js"
   },
   {
     "revision": "de2e58b4d13fcaff2553",
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.237545ef569fc0253ba2229624a05925.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d77767b4b108fb7044a2ab8ba439cb5e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
