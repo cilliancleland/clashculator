5d37cff03a9cc3abbc45bef0ffd84a3c03fcb0ea
cillian cleland <cilliancleland@gmail.com>
2025-02-17 09:33:49 +1100

    oops

---
+++ precache-manifest.362363e96e028a51482435ecddd1674e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc166752a8b7a9690c7",
+    "url": "css/app.53533aa0.css"
+  },
+  {
+    "revision": "edc915b3769649bb29bc1deb6b8c125c",
+    "url": "index.html"
+  },
+  {
+    "revision": "4cc166752a8b7a9690c7",
+    "url": "js/app.936461fb.js"
+  },
+  {
+    "revision": "de2e58b4d13fcaff2553",
+    "url": "js/chunk-vendors.745f2ce4.js"
+  },
+  {
+    "revision": "e53898dd62fec8fbaa2315bab9d0a170",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "6978a616c585d03cb5b542a891995efb",
+    "url": "robots.txt"
+  }
+]);
